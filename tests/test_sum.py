85bc0fa529374416d7507b9bbb30a3e73203b97f
Sakshi semalti <57029133+sakshisemalti@users.noreply.github.com>
2025-11-27 22:05:25 +0530

    AI refactor + style fix on tests/test_sum.py

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,7 +1,5 @@
-
-The error message "no tests ran in 0.00s" indicates that the test suite was not able to find any test cases to run. This is likely because the test file is not located in a directory called "tests", as specified by the line `from ./tests import *`.
-
-To fix this, you can try moving the test file into a directory called "tests" and updating the import statement accordingly. For example:
+# AI refactor applied
+  Here is the corrected code:
 ```
 # AI refactor applied
 
@@ -14,18 +12,5 @@ def add(a, b):
 if __name__ == '__main__':
     test_add()
 ```
-Alternatively, you can specify the absolute path to the test file in the import statement. For example:
-```
-# AI refactor applied
-
-def test_add():
-    assert add(2, 3) == 5
-
-def add(a, b):
-    return a + b
-
-if __name__ == '__main__':
-    test_add()
-```
-In this case, the `from` clause is replaced with an absolute path to the test file. This allows the test suite to find and run the test cases even if they are not located in a directory called "tests".
+The `from sum import add` line is not needed as the `add` function is defined in the same file. The assert statement should be used with a boolean expression, and the `==` operator should be used to compare the result of the `add` function to 5, not the string "5".
 
